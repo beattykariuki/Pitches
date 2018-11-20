9d05a053ea76f3d30b5e926609aa9663bf020507
beattykariuki <bettykariuki026@gmail.com>
2018-11-20 18:59:50 +0300

    imported a user from the .models file

---
+++ app/auth/views.py
@@ -0,0 +1,48 @@
+from flask import render_template, redirect, url_for, flash,request
+from . import auth
+from ..models import User
+from .forms import Registrationform, Loginform
+from ..import db    
+from flask_login import login_user,login_required.logout_user
+from ..email import mail_message
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+    if user is not None and user.verify_password(login_form.password.data):
+        login_user(user,login_form.remember.data)
+        return redirect(request.args.get('next') or url_for('main.index'))
+
+    flash('Invalid username or Password')
+
+    title = "Pitch Login"
+    return render_template('auth/login.html',login_form = login_form, title = title)
+
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = Registrationform()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Hello Welcome to Pitch","email/welcome_user",user.email,user = user)
+
+        return redirect(url_for('authlogin'))
+        title = "New account"
+
+    return render_template('auth/register.html',registartion_form = form)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.idex"))
+   
+
+
+
+
