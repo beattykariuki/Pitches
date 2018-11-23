b82db95f40e8fe456bf8441dd754ac37d1eb9490
beattykariuki <bettykariuki026@gmail.com>
2018-11-23 12:46:05 +0300

    imported render_template from flask

--- app/main/error.py
+++ app/main/error.py
@@ -2,7 +2,7 @@ from flask import render_template
 from . import main
 
 @main.app_errorhandler(404)
-def four_ow_four(error)
+def four_ow_four(error):
 
     return render_template
     from . import main
